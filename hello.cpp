36faadb3c532c2a62146d5c4b4cd4556e504cc5c
tatosen <bourosar12@gmail.com>
2022-03-21 14:31:42 -0400

    Add project files.

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+/*
+Nicholas McCombs
+CIS 1202 R02
+03/21/2022
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World." << endl;
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
